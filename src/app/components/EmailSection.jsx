f01a41fb9b0b992e441d4cf3a9a174d12d2c13aa
unknown <attapon.tun25@gmail.com>
2023-10-18 17:33:52 +0700

    secon commit

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -47,12 +47,12 @@ const EmailSection = () => {
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-1/4 -left-40 transform -translate-x-1/2 -translate-2/2"></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">
-          Let&apos;s Connect
+          Let's Connect
         </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
-          I&apos;m currently looking for new opportunities, my inbox is always
-          open. Whether you have a question or just want to say hi, I&apos;ll
+          I'm currently looking for new opportunities, my inbox is always
+          open. Whether you have a question or just want to say hi, I'll
           try my best to get back to you! <br /><br />
             <li>Gmail : attapon.tun25@gmail.com</li>
             <li>TEL : +66 99 431 2095</li>
