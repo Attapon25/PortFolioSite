52bf286b9148c22781a745f36023a4f9fe4259a8
unknown <attapon.tun25@gmail.com>
2023-10-18 17:20:47 +0700

    first commit

---
+++ src/app/components/AchievementsSection.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const achievementsList = [
+  {
+    metric: "Projects",
+    value: 3,
+    postfix: "",
+  },
+  {
+    prefix: "~",
+    metric: "Users",
+    value: 1,
+  },
+  {
+    metric: "Awards",
+    value: NaN,
+  },
+  {
+    metric: "Years",
+    value: 2,
+  },
+];
+
+const AchievementsSection = () => {
+  return (
+    <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
+        {achievementsList.map((achievement, index) => {
+          return (
+            <div
+              key={index}
+              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
+            >
+              <h2 className="text-white text-4xl font-bold">
+                {achievement.prefix && `${achievement.prefix} `}
+                {achievement.value}
+                {achievement.postfix && `${achievement.postfix} `}
+              </h2>
+              <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default AchievementsSection;
---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,101 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Web scraping Shopee",
+    description: "web scraping Shopee using puppeteer to csv",
+    image: "/images/projects/7.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/Attapon25/web_scraping_Shopee_using_puppeteer_to_csv",
+    previewUrl: "/",
+  },
+  {
+    id: 2,
+    title: "Lalavel 9 CRUD",
+    description: "Basic Lalavel 9 CRUD Project",
+    image: "/images/projects/2.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/Attapon25/lalavel9-crud",
+    previewUrl: "/",
+  },
+  {
+    id: 3,
+    title: "Boxing queue booking Application",
+    description: "Application for booking boxing queues within the gym",
+    image: "/images/projects/4.png",
+    tag: ["All", "Mobile"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectsSection;
